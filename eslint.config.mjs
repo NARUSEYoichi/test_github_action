8b4516d28a2a9b5f6e2743d77ae69ba7a5718e31
Yoichi NARUSE <yoichi_naruse@tatsuno-gr.com>
2025-02-13 14:33:59 +0900

    Add eslint config.

---
+++ eslint.config.mjs
@@ -0,0 +1,62 @@
+import globals from "globals";
+// import pluginJs from "@eslint/js";
+import html from "eslint-plugin-html"
+import autofixPlugin from 'eslint-plugin-autofix';
+
+/** @type {import('eslint').Linter.Config[]} */
+export default [
+    {
+        ignores: [
+            "src/docs/_build/",
+            "src/static/AdminLTE-master/",
+            "src/static/admin/",
+            "src/static/bootstrap-datepicker-1.9.0-dist/",
+            "src/static/jazzmin/",
+            "src/static/vendor/",
+        ]
+    },
+    {
+        plugins: {
+            autofix: autofixPlugin,
+        },
+        files: ["src/**/*.js"],
+        rules: {
+            "no-undef": "error",             // Error on variables not defined with var/let/const
+        },
+    },
+    {
+        languageOptions: {
+            globals: {
+                ...globals.browser,
+                $: "readonly", // Mark jQuery $ as a known global
+                jQuery: "readonly",
+                Swal: "readonly",
+                gettext: "readonly",
+            },
+        },
+        files: ["src/**/*.html"],
+
+        plugins: {html},
+
+        "rules": {
+            "no-undef": "error",             // Error on variables not defined with var/let/const
+        }
+    },
+    {
+        languageOptions: {
+            globals: {
+                ...globals.browser,
+                $: "readonly", // Mark jQuery $ as a known global
+                jQuery: "readonly",
+                Swal: "readonly",
+                gettext: "readonly",
+            },
+        },
+        files: ["src/**/*.js"],
+        rules: {
+            "no-undef": "error",             // Error on variables not defined with var/let/const
+        },
+    },
+
+    // pluginJs.configs.recommended,
+];
